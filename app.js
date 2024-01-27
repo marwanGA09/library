80b450fe8460dfb20c7d9d314647a5c0515ca615
Adem Kedir <ademkedir724@gmail.com>
2024-01-28 01:27:08 +0300

    create basic global function
    - constructor for an object (Book)
    - createBook
    - displayListBook
    - deleteListBook
    - an array used to store book(listOfBook)

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 console.log("hy");
+
+// create book object
+// const Book = {
+//   names,
+//   author,
+//   page,
+//   isRead,
+// };
+
+function Book(name, author, page, isRead) {
+  this.name = name;
+  this.author = author;
+  this.page = page;
+  this.isRead = isRead;
+}
+
+const listOfBook = [];
+
+function createBook(n, a, p, i) {
+  let book = new Book(n, a, p, i);
+  listOfBook.push(book);
+}
+
+function displayListBook() {
+  listOfBook.forEach((e) => console.log(e));
+}
+
+function deleteListBook(index) {
+  console.log(listOfBook.splice(index, 1));
+}
+
+// createBook("asa", "bb", 34, true);
+// createBook("ddd", "bb", 34, true);
+// createBook("qqq", "bb", 34, true);
+// createBook("vvv", "bb", 34, true);
+// displayListBook();
+// deleteListBook(1);
+// displayListBook();
