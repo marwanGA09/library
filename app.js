dfb318ad74aae6290610c74b54768a23e61ddfd2
Adem Kedir <ademkedir724@gmail.com>
2024-01-28 17:02:22 +0300

    create dialog box to add book
    - create form to add book
    - make dialog box to display when + clicked
    - make dialog box to hidden when submit or x is clicked

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ function Book(name, author, page, isRead) {
   this.isRead = isRead;
 }
 
+Book.prototype.pages = function () {
+  console.log(this.page);
+};
+console.log(Book.prototype);
+
 const listOfBook = [];
 
 function createBook(n, a, p, i) {
@@ -36,4 +41,27 @@ function deleteListBook(index) {
 // createBook("vvv", "bb", 34, true);
 // displayListBook();
 // deleteListBook(1);
+
+// listOfBook[1].pages();
 // displayListBook();
+
+const addBook = document.querySelector(".add-book");
+const modal = document.querySelector(".modal");
+const cancelModal = document.querySelector(".cancel-modal");
+addBook.addEventListener("click", (ev) => {
+  modal.showModal();
+});
+
+cancelModal.addEventListener("click", (ev) => {
+  modal.close();
+});
+
+const submitBtn = document.querySelector(".submit-btn");
+submitBtn.addEventListener("click", (ev) => {
+  //   const input = document.querySelectorAll('input[type="text"]');
+  //   input.forEach((inp) => {
+  //     console.log(inp);
+  // //   });
+  //   modal.close();
+  //   console.log(ev.returnValue);
+});
