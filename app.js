369a4163e18a92a9deeabcf40d2ea947d5407c66
Adem Kedir <ademkedir724@gmail.com>
2024-01-29 08:16:12 +0300

    style color

--- app.js
+++ app.js
@@ -44,10 +44,6 @@ createBook("Alchemy", "caylo", 124, "yes", 117);
 createBook("atomic habit", "some one", 134, "no", 0);
 createBook("Alchemy", "caylo", 124, "yes", 117);
 createBook("atomic habit", "some one", 134, "no", 0);
-createBook("poor dad", "robert", 134, "yes", 17);
-createBook("Alchemy", "caylo", 124, "yes", 117);
-createBook("atomic habit", "some one", 134, "no", 0);
-createBook("The  miracle of", "ahmed", 334, "no", 0);
 displayListBook();
 
 // listOfBook[1].pages();
